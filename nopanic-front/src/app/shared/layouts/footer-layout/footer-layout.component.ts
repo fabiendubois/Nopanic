af9f9bc1bfc1fa73f0087304e115e8441b11d8fa
DUBOIS Fabien <fabiendubois2304@gmail.com>
2020-04-15 15:36:28 +0200

    feat : récupérer le nom et la version de l'app depuis le package.json

--- nopanic-front/src/app/shared/layouts/footer-layout/footer-layout.component.ts
+++ nopanic-front/src/app/shared/layouts/footer-layout/footer-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../../../../environments/environment'
 @Component({
   selector: 'app-footer-layout',
   templateUrl: './footer-layout.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterLayoutComponent implements OnInit {
 
+  applicationName = environment.name;
+  applicationVersion = environment.version;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
--- nopanic-front/src/environments/environment.prod.ts
+++ nopanic-front/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+import { name, version } from '../../package.json';
+
 export const environment = {
   production: true,
-  api: 'https://haxhufynk0.execute-api.eu-west-1.amazonaws.com/dev'
+  api: 'https://haxhufynk0.execute-api.eu-west-1.amazonaws.com/dev',
+  name,
+  version
 };
